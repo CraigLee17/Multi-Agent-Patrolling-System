144158579c0ed22f1ff56d4de778f0bde704d2fc
zhiyuan li <li.zhiyuan@uwlax.edu>
2017-02-23 17:59:50 -0600

    Html page updated.

---
+++ MultiAgent/out/artifacts/MultiAgent_war_exploded/js/styleControl.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Zhiyuan Li on 2017/2/21.
+ */
+function toGraphicalView() {
+    $("#blockView").hide();
+    $("#graphicalView").show();
+}
+
+function toBlockView() {
+    $("#blockView").show();
+    $("#graphicalView").hide();
+}
--- MultiAgent/web/js/createBlock.js
+++ MultiAgent/web/js/createBlock.js
@@ -1,12 +1,20 @@
 /**
  * Created by Zhiyuan Li on 2017/2/5.
+ * Edited by Cameron Ario on 2017/2/10.
  */
-var agents = new Array();
-var agentsNumber = new Array();
-var BlockMapArray = new Array();
-var paper
-var textArr = new Array();
+var agents; //array of agents on the map
+var agentsNumber; //array of text for squares with more than one agent on them
+var plainMapArray; //2D array on 1s and 0s representing the map
+var blockMapArray; //2D array of squares in the block viow
+var paper //Space on screen for the block view
+var textArr; //also an array of text for squares with more than one agent on them
+var regionArr; // array of regions, nodes that belong to those regions, and information about those nodes
 
+var graphicalPaper; //Space on the screen for the graph view
+var graphicalMapArray; //2D array of the circles in the graph view
+var graphicalAgentsArr; //array of agents for the graph view
+
+//This code creates/colours the map on the screen and adds agents to the map
 function setUpBlockView(tArray) {
     var container = document.getElementById('blockView');
     if (container.firstChild) {
@@ -15,122 +23,220 @@ function setUpBlockView(tArray) {
     paper = Raphael(container, 8 * 50, 8 * 50);
     var matrix = paper.set();
     var id = 0;
-
+	
+	//colour nodes 
     for (var i = 0; i < 8; i++) {
         matrix[i] = new Array()
         for (var j = 0; j < 8; j++) {
             var element;
-            if (tArray[i][j] == 1) {
-                element = paper.rect(i * 50, j * 50, 50, 50).attr('fill', '#32b6ce').data("flag", 1);
-                element.data("flag", 1);
+            if (tArray[i][j] == 0) {
+                element = paper.rect(i * 50, j * 50, 50, 50).attr('fill', '#32b6ce').data("flag", 0);
+                element.data("flag", 0);
             } else {
-                element = paper.rect(i * 50, j * 50, 50, 50).attr('fill', '#fff').data("flag", 0);//element=square
-                if ((i == 0 && j == 5) || (i == 6 && j == 3) || (i == 6 && j == 4) || (i == 5 && j == 3) || (i == 0 && j == 3) || (i == 0 && j == 4) || (i == 3 && j == 0) || (i == 4 && j == 0) || (i == 7 && j == 3) || (i == 7 && j == 4) || (i == 3 && j == 7) || (i == 4 && j == 7)) {
-                    id++;
-                    var agent = paper.rect(i * 50 + 20, j * 50 + 20, 10, 10)
-                        .attr({
-                            'stroke-width': "0",
-                            "fill": "red"
-                        })
-                        .data({
-                            "x": i,
-                            "y": j,
-                            "id": id
-                        });
-                    element.attr('fill', "#8ffc9c");
-                    agents.push(agent);
-                }
+                element = paper.rect(i * 50, j * 50, 50, 50).attr('fill', '#fff').data("flag", 1);
             }
             matrix[i].push(element);
         }
     }
-    BlockMapArray = matrix;
+	
+	blockMapArray = matrix;
+	
+	//insert random agents
+	for(var a = 0; a < regionArr.length; a++){	
+		var newAgents = Math.floor((Math.random() * Math.floor(regionArr[a].length/2)) + 1); //get a number of new agents for the region between 1 and half of the open nodes
+		for(var b = 0; b < newAgents; b++){
+			var loc = Math.floor((Math.random() * regionArr[a].length)); //get a random node from the Region array
+			var i = regionArr[a][loc][0];
+			var j = regionArr[a][loc][1];
+			
+			changeColor(i, j); //set node to visited
+			
+			id++;
+			
+			var agent = paper.rect(i * 50 + 20, j * 50 + 20, 10, 10) //add agent to map and agents array
+                .attr({
+                    'stroke-width': "0",
+                    "fill": "red"
+                })
+                .data({
+                    "x": i,
+                    "y": j,
+                    "id": id,
+					"region": a,
+					"goal": loc
+                });
+				
+            agents.push(agent);
+			
+			regionArr[a][loc][2][0] += 1; //save number of agent on this node at step 0
+		}
+	}
+	
+	cleanNumber();
+    moreThanOneAgent();
 }
 
+//this method creates the map to be used
 function maps() {
-    var tArray = new Array();
+	agents = new Array();
+	graphicalAgentsArr = new Array();
+	agentsNumber = new Array();
+	plainMapArray = new Array();
+	textArr = new Array();
+	regionArr = new Array();
+	
+	//TODO: pull map from database
+	
+	//STUB: creates static map
     for (var k = 0; k < 8; k++) {
-        tArray[k] = new Array();
+        plainMapArray[k] = new Array();
         for (var j = 0; j < 8; j++) {
-            tArray[k][j] = 0;
+            plainMapArray[k][j] = 1;
         }
     }
     for (var k = 0; k < 8; k++) {
         for (var j = 0; j < 8; j++) {
             if (k == j || k + j == 7) {
-                tArray[k][j] = 1;
+                plainMapArray[k][j] = 0;
             }
         }
     }
-    setUpBlockView(tArray);
-    setUpRegion(tArray);
+	
+	//TODO: add nodes from map that belong to the different regions
+	
+	//STUB: add nodes of static map
+	regionArr[0] = new Array();
+	regionArr[1] = new Array();
+	regionArr[2] = new Array();
+	regionArr[3] = new Array();
+	
+	for(var a = 0; a < 8; a++){
+		for(var b = 0; b < 8; b++){
+			var node = new Array();
+				node.push(a); //node's x position
+				node.push(b); //node's y position
+				node.push([0]); //array containing what agents on it each step of the algorithm (eg. reigonArr[region][node][2][step#])
+				node.push(false); //String true or false if the node has been visited by an agent before
+			
+			if(a == b){}
+			else if((a < 3 && b > 2 && b < 5) || (a < 2 && b > 1 && b < 6) || (a < 1 && b > 0 && b < 7)){
+				
+				regionArr[0].push(node);
+			}
+			else if((b < 3 && a > 2 && a < 5) || (b < 2 && a > 1 && a < 6) || (b < 1 && a > 0 && a < 7)){
+				
+				regionArr[1].push(node);
+			}
+			else if((a > 4 && b > 2 && b < 5) || (a > 5 && b > 1 && b < 6) || (a > 6 && b > 0 && b < 7)){
+				
+				regionArr[2].push(node);
+			}
+			else if((b > 4 && a > 2 && a < 5) || (b > 5 && a > 1 && a < 6) || (b > 6 && a > 0 && a < 7)){
+				
+				regionArr[3].push(node);
+			}
+		}
+	}
+	
+    setUpBlockView(plainMapArray);
+	setUpRegion(plainMapArray);
 }
 
-var copyDirection = new Array();
-function run() {
-    copyDirection.splice(0,copyDirection.length);
-    for (var i = 0; i < agents.length; i++) {
-        var direction = Math.round(Math.random() * 3 + 1);
-        copyDirection.push(direction);
-        var agent = agents[i];
-        var x = agent.data("x");
-        var y = agent.data("y");
+//This method moves the agents a single step when run
+function run(runs) {
+	for(var n = 0; n < runs; n++){
+		var copyDirection = new Array();
+		for(var k = 0; k < agents.length; k++){//for each agent
+			copyDirection.push(new Array());
+			copyDirection[copyDirection.length-1].push(agents[k].data("x")); //set the graph view movement to stay in place is there is no movement
+			copyDirection[copyDirection.length-1].push(agents[k].data("y"));
+		}
+		
+		for(var i = 0; i < regionArr.length; i++){ //for each region
+			var finished = true;
+			for(var j = 0; j < regionArr[i].length; j++){//for each node in that region
+			
+				regionArr[i][j][2].push(0); //start new information on each node's current step
+			
+				if(regionArr[i][j][3] == false){ //if there is an unvisited node
+					finished = false;
+				}
+			}
+			for(var k = 0; k < agents.length; k++){//for each agent
+			
+				if(finished == false && agents[k].data("region") == i){ //run algorithm in unfinished region
+					
+					if(regionArr[i][agents[k].data("goal")][3] == true){ //if the agent's goal has been visited, find a new goal
+						//agents[k].data("goal") = Math.floor(Math.random() * unvisitedRegionArr[i].length);
+						var newGoal = 0;
+						var tempNum = 0;
+						for(var a = 0; a < regionArr[i].length; a++){ //for each node in the region
+							if(regionArr[i][a][3] == false){ //if the node isn't visited
+								var temp = Math.random();
+								if(tempNum <= temp){ //randomly pick a new goal from the unvisited nodes
+									newGoal = a;
+								}
+							}
+						}
+						agents[k].data({ //change the agent's goal
+							"goal": newGoal
+						});
+					}
+					
+					//use algorithm to move
+					var graph = new Graph(plainMapArray);
+					
+					var start = graph.grid[agents[k].data("x")][agents[k].data("y")];
+					var end = graph.grid[regionArr[i][agents[k].data("goal")][0]][regionArr[i][agents[k].data("goal")][1]];
+					var result = astar.search(graph, start, end);
+					
+					var x = result[0].x;
+					var y = result[0].y;
+					
+					copyDirection[k][0] = x; //set the graph view movement to where the agent moved to
+					copyDirection[k][1] = y;
+					
+					agents[k].data({ //change the agent's placement on the map
+						"x": x,
+						"y": y
+					}).attr({x: x * 50 + 20, y: y * 50 + 20}).toFront();
+					
+					
+				}
+			}
+		}
+		
+		runGra(copyDirection);
+		
+		for(var i = 0; i < agents.length; i++){//colour all squares agents are in
+			changeColor(agents[i].data("x"), agents[i].data("y"));
+		}
 
-        switch (direction) {
-            case 1:
-                if (x + 1 < 8) {
-                    if (BlockMapArray[x + 1][y].data("flag") == 0) {
-                        x++;
-                        changeColor(x, y);
-                    }
-                }
-                break;
-            case 2:
-                if (y + 1 < 8) {
-                    if (BlockMapArray[x][y + 1].data("flag") == 0) {
-                        y++;
-                        changeColor(x, y);
-                    }
-                }
-                break;
-            case 3:
-                if (x - 1 >= 0) {
-                    if (BlockMapArray[x - 1][y].data("flag") == 0) {
-                        x--;
-                        changeColor(x, y);
-                    }
-                }
-                break;
-            case 4:
-                if (y - 1 >= 0) {
-                    if (BlockMapArray[x][y - 1].data("flag") == 0) {
-                        y--;
-                        changeColor(x, y);
-                    }
-                }
-                break;
-        }
-        agent.data({
-            "x": x,
-            "y": y,
-        }).attr({x: x * 50 + 20, y: y * 50 + 20}).toFront();
-    }
-    runGra();
+	}
+	
     cleanNumber();
     moreThanOneAgent();
 }
 
+//this method calls the run function n times
+function runNsteps() {
+    var steps = $("#steps").val();
+    
+	run(steps);
+}
+
+//When multiple agents are on the same square, write the number in the square
 function moreThanOneAgent() {
-    var agent, agentFollow;
-    for (var i = 0; i < agents.length; i++) {
-        var count = 1;
+    var agent,agentFollow;
+    for (var i = 0; i < agents.length; i++) {// compare all agents to each other,
+        var count=1;
         for (var j = 0; j < agents.length; j++) {
-            if (i == j) {
+            if ((i == j) || (agents[i].data("region") != agents[j].data("region"))){//if it is the same agent or not in the same region, skip
                 continue;
             }
             agent = agents[i];
             agentFollow = agents[j];
-            if (agent.data("x") == agentFollow.data("x") && agent.data("y") == agentFollow.data("y")) {
+            if (agent.data("x") == agentFollow.data("x") && agent.data("y") == agentFollow.data("y")){ //if agents are on the same square
                 count++;
             }
         }
@@ -138,145 +244,105 @@ function moreThanOneAgent() {
     }
     console.log(agentsNumber)
 
-    for (var k = 0; k < agents.length; k++) {
-        if (agentsNumber[k] >= 1) {
-            var x = agents[k].data('x');
-            var y = agents[k].data('y');
-            var textEle = paper.text(x * 50 + 5, y * 50 + 5, agentsNumber[k]).toFront();
+    for (var k=0;k<agents.length;k++){ //for all agents, write number of agents on square
+        if (agentsNumber[k]>=1){
+            var x=agents[k].data('x');
+            var y=agents[k].data('y');
+            var textEle=paper.text(x*50+5,y*50+5,agentsNumber[k]).toFront();
             textArr.push(textEle);
         }
     }
-    agentsNumber.splice(0, agentsNumber.length)
+    agentsNumber.splice(0,agentsNumber.length)
+
 }
 
+//Remove text from squares without more than one agent
 function cleanNumber() {
-    for (var i in textArr) {
+    for (i in textArr){
         textArr[i].remove();
     }
 }
 
+//change colour of visited square and mark square as visited.
 function changeColor(i, j) {
-    BlockMapArray[i][j].attr("fill", "#8ffc9c");
+    blockMapArray[i][j].attr("fill", "#8ffc9c"); //colour the visited square on the block view
+	
+	for(var k = 0; k < regionArr.length; k++){ //set the node in the region array to visited and add information about agent on that node on the current step
+		for(var l = 0; l < regionArr[k].length; l++){
+			if(regionArr[k][l][0] == i && regionArr[k][l][1] == j){
+				regionArr[k][l][2][regionArr[k][l][2].length-1] += 1;
+				
+				regionArr[k][l][3] = true;
+			}
+		}
+	}
 }
 
 
+//--------------------------------------------------------------------------------------------------------------------
 
-
-
-//-----------------------------------------------------------------------------------------------------------------------------------------
-function changeGraphicalColor(i, j) {
-    graphicalMapArray[i][j].attr("fill", "#8ffc9c");
-}
-
-var graphicalPaper;
-var graphicalMapArray;//circle;
-var graphicalAgentsArr=new Array();
-function setUpRegion(tArray) {//
-    var container = document.getElementById('graphicalView');
+function setUpRegion(tArray){
+	var container = document.getElementById('graphicalView');
     if (container.firstChild) {
         container.removeChild(container.firstChild);
     }
     graphicalPaper = Raphael(container, 8 * 50, 8 * 50);
     var matrix = graphicalPaper.set();
+	
+	//colour nodes 
     for (var i = 0; i < 8; i++) {
         matrix[i] = new Array()
         for (var j = 0; j < 8; j++) {
             var element;
-            if (tArray[i][j] == 1) {
-                element = graphicalPaper.rect(i * 50, j * 50, 50, 50).data("flag", 1).hide();
-                element.data("flag", 1);
+            if (tArray[i][j] == 0) {
+                element = graphicalPaper.rect(i * 50, j * 50, 50, 50).data("flag", 0).hide();
+                element.data("flag", 0);
             } else {
-                element = graphicalPaper.circle(i * 50 + 25, j * 50 + 25, 25).data("flag", 0);
+                element = graphicalPaper.circle(i * 50 + 25, j * 50 + 25, 25).data("flag", 1);
                 graphicalPaper.text(i * 50 + 25, j * 50 + 25,"("+i+","+j+")").toFront();
-                if ((i == 0 && j == 5) || (i == 6 && j == 3) || (i == 6 && j == 4) || (i == 5 && j == 3) || (i == 0 && j == 3) || (i == 0 && j == 4) || (i == 3 && j == 0) || (i == 4 && j == 0) || (i == 7 && j == 3) || (i == 7 && j == 4) || (i == 3 && j == 7) || (i == 4 && j == 7)) {
-                    var graphicalAgent=graphicalPaper.rect(i * 50 + 20, j * 50 + 20, 10, 10)
-                        .attr({
-                            'stroke-width': "0",
-                            "fill": "red"
-                        })
-                        .data({
-                            "x": i,
-                            "y": j,
-                        });
-
-                    element.attr('fill', "#8ffc9c");
-                    graphicalAgentsArr.push(graphicalAgent);
-                }
             }
             matrix[i].push(element);// push circle
         }
     }
+	
     graphicalMapArray = matrix;
+	
+	//insert agents into graph view at positions identical to block view's agents
+	for(var i = 0; i < agents.length; i++){
+		var x = agents[i].data("x");
+		var y = agents[i].data("y");
+		
+		var graphAgent = graphicalPaper.rect(x * 50 + 20, y * 50 + 20, 10, 10)
+                .attr({
+					'stroke-width': "0",
+                    "fill": "red"
+                })
+                .data({
+                    "x": x,
+                    "y": y,
+                });
+				
+		changeGraphicalColor(x, y);
+		graphicalAgentsArr.push(graphAgent);
+	}
 }
 
-function runGra() {
-    for (var i = 0; i < graphicalAgentsArr.length; i++) {
-        var direction = copyDirection[i];
-        var agent = graphicalAgentsArr[i];
-        var x = agent.data("x");
-        var y = agent.data("y");
-
-        switch (direction) {
-            case 1:
-                if (x + 1 < 8) {
-                    if (graphicalMapArray[x + 1][y].data("flag") == 0) {
-                        x++;
-                        changeGraphicalColor(x,y);
-                    }
-                }
-                break;
-            case 2:
-                if (y + 1 < 8) {
-                    if (graphicalMapArray[x][y + 1].data("flag") == 0) {
-                        y++;
-                        changeGraphicalColor(x,y);
-
-                    }
-                }
-                break;
-            case 3:
-                if (x - 1 >= 0) {
-                    if (graphicalMapArray[x - 1][y].data("flag") == 0) {
-                        x--;
-                        changeGraphicalColor(x,y);
-
-                    }
-                }
-                break;
-            case 4:
-                if (y - 1 >= 0) {
-                    if (graphicalMapArray[x][y - 1].data("flag") == 0) {
-                        y--;
-                        changeGraphicalColor(x,y);
-
-                    }
-                }
-                break;
-        }
-        agent.data({
+//move agents in graph view based on thier movements from the block view
+function runGra(copyDirection){
+	for(var i = 0; i < graphicalAgentsArr.length; i++){
+		var x = copyDirection[i][0];
+		var y = copyDirection[i][1];
+		
+		changeGraphicalColor(x, y);
+		
+		graphicalAgentsArr[i].data({
             "x": x,
-            "y": y,
+            "y": y
         }).attr({x: x * 50 + 20, y: y * 50 + 20}).toFront();
-    }
+	}
 }
 
-//-----------------------------------------------------run N steps------------------------------------------------------------------------------------------
-
-function runNsteps() {
-    var steps = $("#steps").val();
-    for (var i = 0; i<steps;i++){
-        run();
-    }
+//change colour of visited circle on graph view
+function changeGraphicalColor(i, j) {
+    graphicalMapArray[i][j].attr("fill", "#8ffc9c"); //colour the visited circle on the graph view
 }
-
-
-
-
-
-
-
-
-
-
-
-
